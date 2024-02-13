bfb645d8d9f72ecbe6a4e99074d5042b63b46ea2
network-charles <charlesniklaus@gmail.com>
2024-02-13 15:21:45 +0100

    Lab 1

--- Lab 1/compute.tf
+++ Lab 1/compute.tf
@@ -1,26 +1,3 @@
-/*
-# Create 2 ec2 container instances 
-resource "aws_instance" "container" {
-  count                       = 3
-  ami                         = data.aws_ami.amazon.id
-  instance_type               = "t3.small"
-  subnet_id                   = count.index % 3 == 0 ? aws_subnet.my_subnet_1.id : count.index % 3 == 1 ? aws_subnet.my_subnet_2.id : aws_subnet.my_subnet_3.id
-
-  vpc_security_group_ids      = [aws_security_group.sg.id]
-  associate_public_ip_address = true
-  iam_instance_profile        = aws_iam_instance_profile.ec2.name
-  # amazon linux rhel 
-  #user_data = file("${path.module}/k8s_amazon_rhel.sh")
-  # ubuntu 
-  user_data = file("${path.module}/k8s_ubuntu.sh")
-  tenancy   = "default"
-
-  tags = {
-    Name = "container-${count.index}"
-  }
-}
-*/
-
 # Create a launch template for ASG 
 resource "aws_launch_template" "lt" {
   image_id               = data.aws_ami.amazon.id
